3c47d704e4df228ff2c592812dd88f0648a8d25a
Mukhammad Sobirov <mukhammadsobirov.ms@gmail.com>
2023-01-22 16:37:34 +0300

    #2.2: create head js for dashboard home page

---
+++ src/app/dashboard/main/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Budget App | Home</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Keep track of your budget" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
