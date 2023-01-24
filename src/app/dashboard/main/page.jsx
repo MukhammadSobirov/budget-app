193b299f8488a1168882874dcea4a53bea6d8949
Mukhammad Sobirov <mukhammadsobirov.ms@gmail.com>
2023-01-24 22:29:23 +0300

    add a stat widget

--- src/app/dashboard/main/page.jsx
+++ src/app/dashboard/main/page.jsx
@@ -1,3 +1,37 @@
+"use client";
+import { Container } from "@mui/material";
+import { Stack } from "@mui/system";
+import { Balance } from "@mui/icons-material";
+import IndeterminateCheckBoxIcon from "@mui/icons-material/IndeterminateCheckBox";
+import StatWidget from "@/components/StatWidget";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+
 export default function DashboardPage() {
-  return <h1>Dahsboard</h1>;
+  return (
+    <Container>
+      <Stack direction={"row"} gap="20px" alignItems="center" justifyContent={"center"} flexWrap={"wrap"}>
+        <StatWidget
+          title={"Balance"}
+          value="100.00"
+          icon={<Balance sx={{ color: "white" }} />}
+          color="teal"
+          currency="$"
+        />
+        <StatWidget
+          title={"Expense"}
+          value="123,345.00"
+          icon={<IndeterminateCheckBoxIcon sx={{ color: "white" }} />}
+          color="tomato"
+          currency="$"
+        />
+        <StatWidget
+          title={"Income"}
+          value="23,000.00"
+          icon={<AddBoxIcon sx={{ color: "white" }} />}
+          color="green"
+          currency="$"
+        />
+      </Stack>
+    </Container>
+  );
 }
---
+++ src/components/StatWidget.jsx
@@ -0,0 +1,31 @@
+import { Paper, Typography } from "@mui/material";
+import { Box, Stack } from "@mui/system";
+
+const StatWidget = ({ title, value, icon, color, currency }) => {
+  return (
+    <Paper sx={{ padding: "15px 20px", width: "250px", height: "150px" }}>
+      <Stack direction={"row"} justifyContent="space-between" alignItems="center" mb={2}>
+        <Stack>
+          <Typography variant="body1">{title}</Typography>
+        </Stack>
+        <Box
+          borderRadius={"50px"}
+          bgcolor={color}
+          width={"50px"}
+          height="50px"
+          alignItems={"center"}
+          justifyContent="center"
+          display="flex"
+        >
+          {icon}
+        </Box>
+      </Stack>
+      <Typography variant="h4">
+        {currency}
+        {value}
+      </Typography>
+    </Paper>
+  );
+};
+
+export default StatWidget;
