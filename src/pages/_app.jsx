8c5577013b3e7201e873fc898b829741ba511f6a
Mukhammad Sobirov <mukhammadsobirov.ms@gmail.com>
2023-01-31 11:44:57 +0300

    install redux and create store

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -10,6 +10,8 @@ import { registerChartJs } from "@/lib/charts-config";
 import { SessionProvider } from "next-auth/react";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { Provider as ReduxProvider } from "react-redux";
+import store from "@/redux/store";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -22,18 +24,20 @@ export default function MyApp(props) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <CacheProvider value={emotionCache}>
-      <SessionProvider session={pageProps.session}>
-        <Head>
-          <meta name="viewport" content="initial-scale=1, width=device-width" />
-        </Head>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          {getLayout(<Component {...pageProps} />)}
-          <ToastContainer />
-        </ThemeProvider>
-      </SessionProvider>
-    </CacheProvider>
+    <ReduxProvider store={store}>
+      <CacheProvider value={emotionCache}>
+        <SessionProvider session={pageProps.session}>
+          <Head>
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+          </Head>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {getLayout(<Component {...pageProps} />)}
+            <ToastContainer />
+          </ThemeProvider>
+        </SessionProvider>
+      </CacheProvider>
+    </ReduxProvider>
   );
 }
 
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+const { configureStore } = require("@reduxjs/toolkit");
+
+const store = configureStore({
+  reducer: {
+    // Add your reducers here
+  },
+});
+
+export default store;
