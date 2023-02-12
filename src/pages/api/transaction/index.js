84d487461df8643654d73ea40996e8f8a7cf435f
Mukhammad Sobirov <mukhammadsobirov.ms@gmail.com>
2023-02-12 18:41:05 +0300

    implement transaction crud

--- src/pages/api/transaction/index.js
+++ src/pages/api/transaction/index.js
@@ -94,7 +94,6 @@ export default async function handler(req, res) {
 
         return res.status(200).json({ transaction: updatedTransaction });
       } catch (error) {
-        console.log(error);
         return res.status(500).json({ message: "Internal server error" });
       }
 
