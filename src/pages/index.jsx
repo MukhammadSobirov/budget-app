cd7f63d08c255cd8bb742798a89378fd2a1e043a
Mukhammad Sobirov <mukhammadsobirov.ms@gmail.com>
2023-02-13 23:24:21 +0300

    fix: minor

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,8 +21,8 @@ const Login = () => {
 
   const formik = useFormik({
     initialValues: {
-      email: "demo@devias.io",
-      password: "Password123",
+      email: "",
+      password: "",
     },
     validationSchema: Yup.object({
       email: Yup.string().email("Must be a valid email").max(255).required("Email is required"),
